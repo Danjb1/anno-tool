c46bdd95851a490c48647d40063820ba025b352b
DanQ <admin@danjb.com>
2025-11-14 09:15:08 +0000

    Check for Anno directory

--- README.md
+++ README.md
@@ -4,7 +4,18 @@ A tool for parsing and editing the files of Anno 1602.
 
 ## Build
 
-```
-cmake -S . -B build
-cmake --build build
-```
+1. Install [CMake](https://cmake.org/) and [vcpkg](https://learn.microsoft.com/en-gb/vcpkg/get_started/get-started?pivots=shell-powershell#1---set-up-vcpkg).
+
+1. Set the `VCPKG_ROOT` environment variable to your vcpkg root directory.
+
+1. Configure the project:
+
+    ```
+    cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
+    ```
+
+1. Build using your desired IDE or by running:
+
+    ```
+    cmake --build build
+    ```
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,54 @@
-#include <iostream>
+#include <boost/program_options.hpp>
 
-int main(int argc, char *argv[]) {
-    std::cout << "hello world!\n";
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
+{
+    // Define accepted program options
+    po::options_description desc("Allowed options");
+    desc.add_options()                                                     //
+            ("help", "produce help message")                               //
+            ("anno-dir", po::value<std::string>(), "Anno 1602 directory")  //
+            ;
+
+    // Parse command-line arguments
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // Help text
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    // Check Anno directory
+    auto it = vm.find("anno-dir");
+    if (it == vm.cend())
+    {
+        std::cout << "Missing required argument: anno-dir\n";
+        return 1;
+    }
+    else
+    {
+        std::string annoDir = it->second.as<std::string>();
+        // text.cod is present in both the original game and the History Edition
+        std::filesystem::path filePath = std::filesystem::path(annoDir) / "text.cod";
+        if (std::filesystem::exists(filePath))
+        {
+            std::cout << "Found Anno directory: " << annoDir << "\n";
+        }
+        else
+        {
+            std::cout << "Invalid Anno directory: " << annoDir << "\n";
+            return 1;
+        }
+    }
+
     return 0;
 }
